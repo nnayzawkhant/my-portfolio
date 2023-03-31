040a282693829bcc0dcd476fc9d720cc293fd8e2
nnayzawkhant <nnayzawkhant@gmail.com>
2023-03-31 16:10:56 +0630

    first commit

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -6,15 +6,6 @@ const Form = () => {
   const ref = useRef ();
   const [success, setSuccess] = useState (null);
   const [loading, setLoading] = useState (false);
-  const [chat, setChat] = useState(false);
-
-  const handleClick = () => {
-    if(!chat){
-      setChat(true)
-    } else {
-      setChat(false)
-    }
-  }
 
   const handleSubmit = e => {
     e.preventDefault ();
@@ -30,7 +21,7 @@ const Form = () => {
       .then (
         result => {
           console.log (result.text);
-          setSuccess (true);
+          ~setSuccess (true);
           setLoading (false);
         },
         error => {
@@ -43,13 +34,14 @@ const Form = () => {
   return (
     <form className={`${classes.form}`} ref={ref} onSubmit={handleSubmit}>
       <div className={`${classes.form__group}`}>
-        <input type="text" placeholder="Your Name" name="user_name" required />
+        <input type="text" placeholder="Your Name" name="user_name" pattern= {`^[A-Za-z0-9]{3,16}$`} required />
       </div>
       <div className={`${classes.form__group}`}>
         <input
           type="email"
           name="user_email"
           placeholder="Email Address"
+          pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
           required
         />
       </div>
@@ -62,32 +54,11 @@ const Form = () => {
           required
         />
       </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'flex-start',
-          gap: '1rem',
-          marginLeft: '10px',
-          marginBottom: '30px'
-        }}
-      >
-        <input
-          type="checkbox"
-          className="custom-control-input"
-          id="customCheck1"
-          onClick={handleClick}
-        />
-        <label
-          className="custom-control-label"
-          htmlFor="customCheck1"
-          style={{color: 'white'}}
-        >
-          Remember me
-        </label>
-      </div>
 
-      <button className="primary__btn" type="submit" disabled={!chat}>
-        {loading ? <span>loading...</span> : <span style={{backgroundColor: 'transparent'}}>Send</span>}
+      <button className="primary__btn" type="submit">
+        {loading
+          ? <span>loading...</span>
+          : <span style={{backgroundColor: 'transparent'}}>Send</span>}
       </button>
       <span style={{color: 'white', marginLeft: '20px'}}>
         {success && "Your message has been sent. We'll get back to you soon :)"}
